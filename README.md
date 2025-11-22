0e2de443e5c30d0fc07ad985894d24f87ba42667
Samuel Hermany <samuelhermany@hotmail.com>
2025-11-22 01:51:37 -0300

    feat: Readicionado o Swagger

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-# Ajustes no Docker Desktop
+# 1. Informações da Aplicação
+- Porta 8083
+- Banco de dados MySQL
+- Dois containers, um para aplicação(8083) e outro para o banco(3307)
+
+# 2. Eureca
+- Foi comentada no docker-compose.yml pra testar a aplicação contenerizada, basta descomentar e fazer os devidos ajustes nessa parte
+
+# 3. Ajustes no Docker Desktop
 1. Abra Docker Desktop → Settings → Docker Engine.
 2. Altere o JSON adicionando "dns": ["8.8.8.8", "8.8.4.4"], por exemplo:
 ```
@@ -13,3 +21,11 @@
      "dns": ["8.8.8.8", "8.8.4.4"] 
 }
 ```
+
+# 4. Swagger
+- Endereço:  http://localhost:8083/swagger-ui/index.html#/
+- EndPoints:
+    - POST - Enviar EmaIl
+    - GET -  Teste de conexão
+  
+![Descrição](documents/swagger.png)
--- src/main/java/com/blogpessoal/email_service/controller/EmailController.java
+++ src/main/java/com/blogpessoal/email_service/controller/EmailController.java
@@ -2,6 +2,10 @@ package com.blogpessoal.email_service.controller;
 
 import com.blogpessoal.email_service.model.EmailRequest;
 import com.blogpessoal.email_service.service.EmailService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,11 +14,21 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/emails")
+@Tag(name = "Email API", description = "Endpoints para envio e verificação do serviço de e-mail")
 public class EmailController {
 
     @Autowired
     private EmailService emailService;
 
+    @Operation(
+            summary = "Enviar um e-mail",
+            description = "Recebe os dados do e-mail e executa o envio para o destinatário informado."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "E-mail enviado com sucesso."),
+            @ApiResponse(responseCode = "400", description = "Dados inválidos."),
+            @ApiResponse(responseCode = "500", description = "Erro interno ao enviar e-mail.")
+    })
     @PostMapping("/send")
     public ResponseEntity<Map<String, Object>> sendEmail(@RequestBody EmailRequest request) {
         emailService.sendEmail(request);
@@ -27,6 +41,11 @@ public class EmailController {
         return ResponseEntity.ok(response);
     }
 
+    @Operation(
+            summary = "Verificação se aservbiço está ativo",
+            description = "Verifica se o serviço de e-mail está ativo."
+    )
+    @ApiResponse(responseCode = "200", description = "Serviço funcionando corretamente.")
     @GetMapping("/ping")
     public String ping() {
         return "📡 Email Service ativo e funcionando!";
